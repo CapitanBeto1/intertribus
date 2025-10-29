7d5502e751d5be9bc5c84d0e677b94feeac0bf80
Bianco <biancoagus159@gmail.com>
2025-10-29 19:48:19 -0300

    update main.py

---
+++ inscripciones.py
@@ -0,0 +1,24 @@
+from funciones_auxiliares import pedirDatosUsuario, mostrarJuegos
+from funciones_inscribir import inscribir
+
+def inscribirUsuario():
+    nombre, genero, tribu = pedirDatosUsuario()
+    inscripciones = 0
+
+    while inscripciones < 3:
+        mostrarJuegos()
+        juego = input("Selecciona el juego al que deseas inscribirte (o 0 para salir):  ")
+
+        if juego == "0":
+            break
+
+        exito, nombreJuego = inscribir(nombre, tribu, juego)
+
+        if exito:
+            inscripciones += 1
+            print(f"Te inscribiste correctamente en {nombreJuego}. Total inscripciones: {inscripciones}/3")
+        else:
+            print("No hay cupos disponibles para ese juego")
+        
+        if inscripciones == 3:
+            print("Alcanzaste el maximo de inscripciones permitidas (3).")
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 
 
+# menu
+def main(): 
+    while True:
+        print("\n==== Menú Principal ====")
+        print("1. Inscribirse en un juego")
+        print("2. Ver fixture de juegos")
+        print("3. Ver inscriptos por tribu")
+        print("0. Salir")
 
-def main():
-    print("meepo number 1 reporting")
+        opcion = input("Seleccione una opción: ")
 
-
+        if opcion == "1":
+            
 
 
 
